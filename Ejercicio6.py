97c864c4a6a45e4ad88982e02e03ef157ddd9678
danielagrvc <danielagrvc@gmail.com>
2022-12-29 12:27:31 +0100

    Ejercicio 1 tema 5

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -1,35 +1,15 @@
-#De forma iterativa
-import numpy as np
-def construccion(matriz):
-    matriz= np.array([[4,3,2,5,6] , [1,2,7,8,3], [3,8,9,3,1], [2,3,5,6,1], [1,3,4,6,4]])
-    determinante=np.linalg.det(matriz)
-    return determinante
-#De forma recursiva
-class Matriz():
-    def __init__(self,filas: list[list[int]]):
-        self.filas = filas
-        self.filas = []
+import datetime
+import time
+import os
 
-    def columns(self, columnas: list[list[int]]):
-        self.columnas = columnas
-        return [[fila[i] for fila in self.filas] for i in range(len(self.filas[0]))]
+while True:
+    hora_actual = datetime.datetime.now()
+    horas = hora_actual.hour
+    minutos = hora_actual.minute
+    segundos = hora_actual.second
 
-    def numero_filas(self):
-        return len(self.filas)
-    def numero_columnas(self):
-        return len(self.filas[0])
+    print(f"{horas:02}:{minutos:02}:{segundos:02}")
 
-    def order(self):
-        return (self.numero_filas, self.numero_columnas)
-    
-    def determinante(self):
-        if self.order ==(0,0):
-            return 1
-        elif self.order ==(1,1):
-            return int(self.filas[0][0])
-        elif self.orden == (2, 2):
-            return int((self.filas[0][0] * self.filas[1][1])
-                - (self.filas[0],[1]* self.filas[1],[0])
-        else:
-            return sum(self.filas[0][self.filas[0]]* self.filas[0][columna])
-                for columna in range(self.numero_columnas))
+    time.sleep(1)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
